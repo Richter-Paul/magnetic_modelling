c2f8f9f95145c92064f0be71a4e65ff633869f7c
Paul Richter <paul.2004richter@gmail.com>
2024-12-13 21:02:07 -0800

    Final magnetic model values settled. Now plots TMI and down component in separate windows.

--- test.py
+++ test.py
@@ -3,14 +3,12 @@ import matplotlib.pyplot as plt
 import magnets as m
 
 
-# Heatmap Test
-northpos1 = np.array([1, 0, 1])
-southpos1 = np.array([-1, 0, -1])
-m1 = 3e31
-northpos2 = np.array([0, -0.5, 1])
-southpos2 = np.array([0, 0.5, -1])
-m2 = 0
-#m2 = 2e22
+northpos1 = np.array([0, -0.5, 1])
+southpos1 = np.array([0, 0.5, -1])
+m1 = 1.75e31
+northpos2 = np.array([1e6, 1e6, 1])
+southpos2 = np.array([1e6, 1e6, -1])
+m2 = 2e31
 
 lats = np.linspace(-np.pi/2, np.pi/2, 100)
 longs = np.linspace(0, 2*np.pi, 100)
@@ -23,15 +21,20 @@ for i in range(len(lats)):
     for j in range(len(longs)):
         point = np.array([m.EARTH_RAD, lats[i], longs[j]])
         cart[i, j] = m.magnetic_field_cart(northpos1, southpos1, m1, point) + m.magnetic_field_cart(northpos2, southpos2, m2, point)
+        sph[i, j] = m.magnetic_field_sph(northpos1, southpos1, m1, point) + m.magnetic_field_sph(northpos2, southpos2, m2, point)
         TMI[i, j] = m.TMI(cart[i, j])
 
 
-# This flips the x-axis (latitudes) because imshow plots upside down :(
+# This flips the y-axis (longitudes) because imshow plots upside down :(
 flipTMI = np.zeros((len(lats), len(longs)))
 for i in range(len(TMI[0])):
     for j in range(len(TMI[1])):
-        flipTMI[i, j] = TMI[len(TMI[0]) - 1 - i, j]
+        flipTMI[i, j] = TMI[i, len(TMI[1]) - 1 - j]
 
+flipsph = np.zeros((len(lats), len(longs), 2))
+for i in range(len(sph[0])):
+    for j in range(len(sph[1])):
+        flipsph[i, j] = sph[i, len(sph[1]) - 1 - j]
 
 
 
@@ -41,9 +44,24 @@ for i in range(len(TMI[0])):
 img = plt.imread("mercator.jpeg")
 x = range(100)
 
+lev = np.arange(0, 1e6, 5000)
+
 fig, ax = plt.subplots()
 ax.imshow(img, extent=[0, 100, 0, 100])
-cs = ax.contour(flipTMI, levels=[2e4, 4e4, 5e4, 6e4],
+cs = ax.contour(flipTMI, levels=lev,
+    colors=['#FF0000'], extend='both')
+ax.clabel(cs, fontsize=10)
+
+
+
+img = plt.imread("mercator.jpeg")
+x = range(100)
+
+lev = np.arange(-1e6, 1e6, 5000)
+
+fig, ax = plt.subplots()
+ax.imshow(img, extent=[0, 100, 0, 100])
+cs = ax.contour(flipsph[:, :, 0], levels=lev,
     colors=['#FF0000'], extend='both')
 ax.clabel(cs, fontsize=10)
 plt.show()
